345155d9fe1c8cf6f7e8447390c997dd36675959
Wanderson10 <wg21249@gmail.com>
2023-03-01 10:24:32 -0300

    portifolio

---
+++ src/utils/stackData.ts
@@ -0,0 +1,46 @@
+import imgHTML from "../public/static/img/stack/html.svg";
+import imgCSS from "../public/static/img/stack/css.svg";
+import imgJS from "../public/static/img/stack/js.svg";
+import imgNode from "../public/static/img/stack/node.svg";
+import imgStyled from "../public/static/img/stack/styled.svg";
+import imgReact from "../public/static/img/stack/react.svg";
+import imgPython from "../public/static/img/stack/Python.svg"
+import imgDjango from "../public/static/img/stack/django-logo-negative.svg"
+import imgPostgres from "../public/static/img/stack/postgresql-inc.svg"
+import { FaVuejs } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "HTML",
+    img: imgHTML,
+  },
+  {
+    title: "CSS",
+    img: imgCSS,
+  },
+  {
+    title: "JS",
+    img: imgJS,
+  },
+  {
+    title: "Node JS",
+    img: imgNode,
+  },
+  {
+    title: "Styled Components",
+    img: imgStyled,
+  },
+  {
+    title: "React",
+    img: imgReact,
+  },
+  { title: "TypeScript", img: SiTypescript },
+  { title: "Docker", img: FaDocker },
+  {title:"Python", img:imgPython },
+  {title:"Django", img:imgDjango},
+  {title:"Postgres", img:imgPostgres},
+ 
+
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,6 @@
+export const userData = {
+  nameUser: "Wanderson  Guedes",
+  githubUser: "Wanderson10",
+  linkedinUser: "wanderson-corrêa-guedes-51684b246",
+  whatsappNumber: "+5551997866074",
+};
